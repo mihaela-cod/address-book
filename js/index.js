52e57abbd25dd92c0b678db3baebe86db182a7e5
manuelfarcas <114208372+manuelfarcas@users.noreply.github.com>
2022-11-17 14:31:41 +0200

    Update - add to top button

--- js/index.js
+++ js/index.js
@@ -9,6 +9,24 @@ function $(selector) {
   return document.querySelector(selector);
 }
 
+// Preluare Buton:
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // Scroll Up Safari
+  document.documentElement.scrollTop = 0; // Chrome, Opera, Firefox
+}
+
 function getPersonHTML(person) {
   return `
   <tr>
